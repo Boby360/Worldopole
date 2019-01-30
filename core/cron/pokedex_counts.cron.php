a7cd7a74c23be6a3fd043d9c1aa483d48e715b66
Michael Krug <michi.krug@gmail.com>
2019-01-30 10:25:19 +0100

    Use new spawn point table for poked counts

--- core/cron/pokedex_counts.cron.php
+++ core/cron/pokedex_counts.cron.php
@@ -11,31 +11,31 @@ if (is_null($pokecountdatas)) {
 
 $total_pokemon = 0;
 $pokedex_counts = new stdClass();
-for ($pid = 1; $pid <= $maxpid; $pid++) {
+for ($pid = 1; $pid <= $maxpid; ++$pid) {
 	if (!isset($newpokecountdatas[$pid])) {
 		$emptyArray = array(
-			"count" => 0,
-			"last_update" => 0,
-			"disappear_time" => null,
-			"latitude" => null,
-			"longitude" => null
+			'count' => 0,
+			'last_update' => 0,
+			'disappear_time' => null,
+			'latitude' => null,
+			'longitude' => null,
 		);
 		$newpokecountdatas[$pid] = $emptyArray;
 	}
 
 	$last_update = $newpokecountdatas[$pid]['last_update'];
 
-	$where = "WHERE p.pokemon_id = '".$pid."' AND (UNIX_TIMESTAMP(p.disappear_time) - (LENGTH(s.kind) - LENGTH( REPLACE ( kind, \"s\", \"\") )) * 900) > '".$last_update."'";
-	$req = "SELECT count, (UNIX_TIMESTAMP(p.disappear_time) - (LENGTH(s.kind) - LENGTH( REPLACE ( kind, \"s\", \"\") )) * 900) as last_timestamp, (CONVERT_TZ(p.disappear_time, '+00:00', '".$time_offset."')) AS disappear_time_real, p.latitude, p.longitude
+	$where = "WHERE p.pokemon_id = '".$pid."' AND (UNIX_TIMESTAMP(p.disappear_time) - (LENGTH(s.spawndef) - LENGTH( REPLACE ( spawndef, \"s\", \"\") )) * 900) > '".$last_update."'";
+	$req = "SELECT count, (UNIX_TIMESTAMP(p.disappear_time) - (LENGTH(s.spawndef) - LENGTH( REPLACE ( spawndef, \"s\", \"\") )) * 900) as last_timestamp, (CONVERT_TZ(p.disappear_time, '+00:00', '".$time_offset."')) AS disappear_time_real, p.latitude, p.longitude
 		FROM pokemon p
-		JOIN spawnpoint s ON p.spawnpoint_id = s.id
+		JOIN trs_spawn s ON p.spawnpoint_id = s.spawnpoint
 		JOIN (
 			SELECT count(*) as count
 			FROM pokemon p
-			JOIN spawnpoint s ON p.spawnpoint_id = s.id
-			" . $where."
+			JOIN trs_spawn s ON p.spawnpoint_id = s.spawnpoint
+			".$where."
 		) x
-		" . $where . "
+		".$where."
 		ORDER BY last_timestamp DESC
 		LIMIT 0,1";
 	$result = $mysqli->query($req);
@@ -57,10 +57,10 @@ for ($pid = 1; $pid <= $maxpid; $pid++) {
 	}
 }
 
-if (!isset($newpokecountdatas["total"])) {
-	$newpokecountdatas["total"] = 0;
+if (!isset($newpokecountdatas['total'])) {
+	$newpokecountdatas['total'] = 0;
 }
-$newpokecountdatas["total"] += $total_pokemon;
+$newpokecountdatas['total'] += $total_pokemon;
 
 // Write to file
 file_put_contents($pokedex_counts_file, json_encode($newpokecountdatas));
