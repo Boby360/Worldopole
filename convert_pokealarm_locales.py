806a0d365b42ffc7908859f3caceab6b3dc721c1
Michael Krug <michi.krug@gmail.com>
2021-08-24 22:37:03 +0200

    Add conversion scripts to update data

---
+++ convert_pokealarm_locales.py
@@ -0,0 +1,21 @@
+import json
+
+
+with open('core/json/locales/EN/pokes.json') as f:
+    content = json.load(f)
+    pokemon = content.get('pokemon', {})
+
+    with open('en.json') as f2:
+        locale = json.load(f2).get('pokemon', [])
+        for entry in locale:
+            pkm = pokemon.get(str(int(entry)))
+            if pkm is None:
+                pokemon[int(entry)] = {
+                    'name': locale[entry],
+                    'description': ''
+                }
+            else:
+                pkm['name'] = locale[entry]
+
+    with open('core/json/locales/EN/pokes.json', 'w') as outfile:
+        json.dump(content, outfile, indent=2, sort_keys=True)
